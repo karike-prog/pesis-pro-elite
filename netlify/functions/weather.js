f6e98180fcf23147f6267b6ff623ee7bae1d4450
karike-prog <kari.keranen@dnainternet.net>
2026-06-09 16:36:58 +0300

    Update weather.js

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,1 +1,14 @@
-
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      source: "FMI Open Data",
+      temperature: null,
+      windSpeed: null,
+      precipitation: null,
+      windDirection: null,
+      note: "Weather function test OK"
+    })
+  };
+};
