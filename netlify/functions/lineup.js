0572ae0933701ef528fad0b33954ac54d23d2211
karike-prog <kari.keranen@dnainternet.net>
2026-06-12 19:25:47 +0300

    Update lineup.js

--- netlify/functions/lineup.js
+++ netlify/functions/lineup.js
@@ -1,1 +1,33 @@
+exports.handler = async function(event) {
+  const q = event.queryStringParameters || {};
+  const id = q.id;
 
+  if (!id) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "id puuttuu" })
+    };
+  }
+
+  const url =
+    `https://www.pesistulokset.fi/taso/rest/match?id=${id}&apikey=wRX0tTke3DZ8RLKAMntjZ81LwgNQuSN9`;
+
+  try {
+    const r = await fetch(url);
+    const text = await r.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store"
+      },
+      body: text
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message })
+    };
+  }
+};
