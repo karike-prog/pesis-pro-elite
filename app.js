860e263589a4923a9a5bfab50c1da906bf8ac6d7
karike-prog <kari.keranen@dnainternet.net>
2026-06-12 20:14:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -275,7 +275,9 @@ async function fetchLineup(match) {
     const res = await fetch(url);
     if (!res.ok) return null;
 
-    return await res.json();
+    const json = await res.json();
+    console.log("LINEUP JSON:", json);
+    return json;
   } catch (e) {
     console.log("LINEUP VIRHE:", e);
     return null;
