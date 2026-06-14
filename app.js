efbd0c4d0c17dfde4ff1c9fad2b50969e0d4304e
karike-prog <kari.keranen@dnainternet.net>
2026-06-14 15:46:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -625,7 +625,7 @@ ${resultHtml(match, prediction)}
 ${(match.result || match.liveResult?.finished)
     ? ""
     : weatherHtml(weather)}
-keyPlayerAbsenceHtml(match, lineup, selectedSeries)
+${keyPlayerAbsenceHtml(match, lineup, selectedSeries)}
 ${lineupHtml(lineup)}
 
         <div class="reason">${prediction.note}</div>
